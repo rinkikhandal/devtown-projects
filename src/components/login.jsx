9280a66ef0e946cafee9e02f2e2d0cf9f819d924
rinki <rinkikhandal2000@gmail.com>
2023-08-26 12:47:56 +0530

    fixing my blunders no.2

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         setclr("red")
       }
       else {
-        navigate("/todos",{state:{name:name,password:password}})
+        navigate("/todos")
         
       }
 
