f8d49b68c1d8fd7685d4c1cde140d18dfa923567
rinki <rinkikhandal2000@gmail.com>
2023-08-26 14:17:07 +0530

    handleDelete still not stable

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,26 +1,31 @@
 import { RiDeleteBin6Fill } from "react-icons/ri";
 import { BiSolidEdit } from "react-icons/bi";
 import { useNavigate } from "react-router";
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Task = ({ todos }) => {
   const navigate = useNavigate();
+  const [updatedTodos, setUpdatedTodos] = useState(todos);
+
+  useEffect(() => {
+    setUpdatedTodos(todos); // Update updatedTodos whenever todos prop changes
+  }, [todos]);
 
   const handleEdit = (todo) => {
-    navigate(`/editTodo/${todo.id}`, { state: { todos: todos } });
+    navigate(`/editTodo/${todo.id}`, { state: { todos: updatedTodos } });
   };
 
   const handleDelete = (e, todo) => {
     const article = e.currentTarget.parentElement.parentElement;
     article.style.display = "none";
-    const updatedTodos = todos.filter((t) => t.id !== todo.id);
-    navigate("/todos", { state: { updatedTodos: updatedTodos } });
+    const filteredTodos = updatedTodos.filter((t) => t.id !== todo.id);
+    setUpdatedTodos(filteredTodos);
   };
 
   return (
     <div className='w-full flex flex-col items-center'>
-      {todos &&
-        todos.map((todo) => {
+      {updatedTodos &&
+        updatedTodos.map((todo) => {
           return (
             <article className='text-white flex justify-between  items-center  rounded-lg shadow border  bg-gray-800 border-gray-700 w-11/12 max-w-[500px] py-2 px-10 capitalize box-border mb-4 h-full'>
               <p
